329a0bc8ed6380301162e657fa7baf218147c364
CVedaReddy <157104274+CVedaReddy@users.noreply.github.com>
2024-01-22 14:46:00 +0530

    Add files via upload

---
+++ <23mm01007><qsn1>.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a=15;
+      printf("%d", a++);
+    printf("\n%d", ++a);
+    printf("\n%d",--a);
+    printf("\n%d",a--);
+    return 0;
+
+}
+
+/* #include<stdio.h>
+int main(){
+    int a=15;
+printf("%d",a++);
+printf("\n%d",++a);
+printf("\n%d",--a);
+printf("\n%d",a--);
+return 0;
+
+
+}  */
